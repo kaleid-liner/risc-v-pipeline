5a870ffcb1889ec52be36c1eaa3cd0ec7cfca4f2
kalineid <noob@mail.ustc.edu.cn>
2020-04-21 11:40:32 +0800

    Implement memory out data extension

--- Lab2/SourceCode/DataExtend.v
+++ Lab2/SourceCode/DataExtend.v
@@ -33,5 +33,47 @@ module DataExtend(
     );
 
     // TODO: Complete this module
+    reg [31:0] data_before_extend;
+
+    always @(*) begin
+        case (load_type)
+            `LB     : begin
+                case (addr)
+                    2'b00: data_before_extend[7:0] = data[7:0];
+                    2'b01: data_before_extend[7:0] = data[15:8];
+                    2'b10: data_before_extend[7:0] = data[23:16];
+                    2'b11: data_before_extend[7:0] = data[31:24];
+                endcase
+                dealt_data = {{24{data_before_extend[7]}}, data_before_extend[7:0]};
+            end
+            `LBU    : begin
+                case (addr)
+                    2'b00: data_before_extend[7:0] = data[7:0];
+                    2'b01: data_before_extend[7:0] = data[15:8];
+                    2'b10: data_before_extend[7:0] = data[23:16];
+                    2'b11: data_before_extend[7:0] = data[31:24];
+                endcase
+                dealt_data = {24'b0, data_before_extend[7:0]};
+            end
+            `LH    : begin
+                case (addr[1])
+                    0: data_before_extend[15:0] = data[15:0];
+                    1: data_before_extend[15:0] = data[31:16];
+                endcase
+                dealt_data = {{16{data_before_extend[15]}}, data_before_extend[15:0]};
+            end
+            `LHU    : begin
+                case (addr[1])
+                    0: data_before_extend[15:0] = data[15:0];
+                    1: data_before_extend[15:0] = data[31:16];
+                endcase
+                dealt_data = {16'b0, data_before_extend[15:0]};
+            end
+            `LW     : begin
+                dealt_data = data;
+            end
+            default : dealt_data = 0;
+        endcase
+    end
 
 endmodule
